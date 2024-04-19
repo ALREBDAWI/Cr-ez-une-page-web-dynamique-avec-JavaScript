09d3f0cf497a17e1e68e6cb7b790060c4b4bb9ce
ALREBDAWI <mahmoudrebdawi@gmail.com>
2024-04-19 16:42:05 +0200

    adding filters

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -3,25 +3,24 @@ async function getWorks() {
     try {
         const responseWorks = await fetch("http://localhost:5678/api/works")
         .then(responseWorks => responseWorks.json());
-        //console.log(responseWorks);
-        data = responseWorks;generateworks(responseWorks);
-        
-
+        data = responseWorks;
+        generateworks(responseWorks);
+        getCategory(responseWorks)       
     } catch (error) {
         console.error('erreur', error);
         
     }
 }
-getWorks();
 
+getWorks()
 
 
 async function generateworks(works){
+   const gallery = document.querySelector('.gallery');
+   gallery.innerHTML=' ' 
    works.forEach(work =>{
-    
-    console.log(work);
+ 
     const portfolio = document.getElementById('portfolio');
-    const gallery = document.querySelector('.gallery');
     const figure = document.createElement('figure');
     const image = document.createElement('img');
     const figcaption = document.createElement('figcaption');
@@ -33,6 +32,43 @@ async function generateworks(works){
     figcaption.innerText=work.title; 
     
 });
+}
+
+async function getCategory(works){
+    try {
+        const responseCategory = await fetch("http://localhost:5678/api/categories")
+        .then(responseCategory => responseCategory.json());
+        console.log(responseCategory);
+        categoryFilter(works,responseCategory)
+    } catch (error) {
+        console.error('erreur', error);
+        
+    }
+}
+    
+
+
+function categoryFilter(works,categories){
+    const filters = document.getElementById('filter-btns');
+    filters.innerHTML=' '
+    const btns = document.createElement('button')
+    btns.textContent=('Tous')
+    btns.addEventListener('click', ()=>{
+       generateworks(works)
+    });
+    filters.appendChild(btns)
+    categories.forEach(category =>{
+        const btn = document.createElement('button')
+        btn.textContent=category.name
+        btn.onclick=() =>{
+             worksFilter(category.name,works)
+        }
+        filters.appendChild(btn)
+    })
+}
+function worksFilter(categoryname,works){
+    const filterdWorks = works.filter(work => work.category.name === categoryname)
+    generateworks(filterdWorks)
 }
 
 
